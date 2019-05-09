7d5ffb3a7d0e06883ad7f6f26b018ed642ccc64a
weimeng2017 <weimeng2017@cnpc.com.cn>
2019-05-09 16:19:47 +0800

    测试test

---
+++ springboot-first/src/test/java/com/springboot/HelloControllerIT.java
@@ -0,0 +1,44 @@
+package com.springboot;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.net.URL;
+
+/**
+ * @ClassName HelloControllerIT
+ * @Description TODO
+ * @Author 溦林昔
+ * @Date 2019/5/9.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerIT {
+    @LocalServerPort
+    private int port;
+
+    private URL base;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Before
+    public void setUp() throws Exception{
+        this.base = new URL("http://localhost:" + port + "/");
+    }
+
+    @Test
+    public void getHello() throws Exception{
+        ResponseEntity<String> response = template.getForEntity(base.toString(),String.class);
+        assertThat(response.getBody(),equalTo("nosql"));
+    }
+}
